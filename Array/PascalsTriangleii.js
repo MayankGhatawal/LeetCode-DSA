8cf25e48b79aaeec98eb500e1eb87ebcd8ac63a0
MayankGhatawal <mayank1ghatawal@gmail.com>
2025-01-03 13:52:52 +0530

    main

---
+++ Array/PascalsTriangleii.js
@@ -0,0 +1,10 @@
+var getRow = function(rowIndex) {
+    let row = [1];
+
+    for (let i = 0; i < rowIndex; i++) {
+        row = row.map((val, index) => (row[index - 1] || 0) + (row[index] || 0));
+        row.push(1);
+    }
+
+    return row;
+};
